189ee6c70c3913138c484babdc3c6dd4d32d6070
DarkMG73 <levelthreeemail@gmail.com>
2018-11-24 00:53:19 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,25 @@
+$( document).ready(function() {
+
+<div id="stage" >
+  
+  <div id="spinner-wrap" >
+    
+    <div id="spinner" style="-webkit-transform-origin: 180px 0 0;">
+      
+      <img style="-webkit-transform: rotateY(0deg) translateX(180px); padding: 0 0 0 160px;" src="https://source.unsplash.com/collection/367159/200x160?blue" width="200" height="160" alt="">
+
+      <img style="-webkit-transform: rotateY(-72deg) translateX(180px); padding: 0 0 0 147px;" src="https://source.unsplash.com/collection/367159/200x160?" width="213" height="160" alt="">
+
+      <img style="-webkit-transform: rotateY(-144deg) translateX(180px); padding: 0 0 0 120px;" src="https://source.unsplash.com/collection/367159/200x160?grass" width="240" height="160" alt="">
+
+      <img style="-webkit-transform: rotateY(-216deg) translateX(180px); padding: 0 0 0 147px;" src="https://source.unsplash.com/collection/367159/200x160?sky" width="213" height="160" alt="">
+
+      <img style="-webkit-transform: rotateY(-288deg) translateX(180px); padding: 0 0 0 122px;" src="https://source.unsplash.com/collection/367159/200x160?nature,water" width="238" height="160" alt="">
+      
+    </div>
+    
+  </div>
+  
+</div>
+
+}); // END document.ready
